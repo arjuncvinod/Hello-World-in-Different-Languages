15896973f15c5eeeb6010ad47580129daf356aa0
Sakshi Umredkar <128240967+saakshii12@users.noreply.github.com>
2023-10-01 00:53:19 +0530

    Update Java.java

--- Java.java
+++ Java.java
@@ -1,7 +1,7 @@
 import java.io.*;
  
-class GFG {
-    public static void main (String[] args) {
-       System.out.println("Hello World");
+class Hactoberfest {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
 }
