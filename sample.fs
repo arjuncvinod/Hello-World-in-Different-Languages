8d816ee41ff342e96f067a42a6107d4972579dfb
Sushanth Reddy <63182782+sushanth-777@users.noreply.github.com>
2023-10-07 15:18:47 -0400

    Create sample.fs

---
+++ sample.fs
@@ -0,0 +1,6 @@
+// A sample F# program as an indication to my recent recent F# learnings. 
+let main() =
+    printfn "Happy 10th anniversary Hacktoberfest!"
+
+// Call the main function to run the program
+main()
