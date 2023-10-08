8b2b68b174032215b0ff599b72876427a06fb8d8
Karthik Krishna <karthikk1006@gmail.com>
2023-10-08 16:42:50 +0530

    This is a hello world prgram in Pikalang.

---
+++ pikalang.py
@@ -0,0 +1,3 @@
+import pikalang
+msg="pi pi pi pi pi pi pi pi pi pi pika pipi pi pi pi pi pi pi pi pipi pi pi pi pi pi pi pi pi pi pi pipi pi pi pi pipi pi pichu pichu pichu pichu ka chupipi pi pi pikachu pipi pi pikachu pi pi pi pi pi pi pi pikachu pikachu pi pi pi pikachu pipi pi pi pikachu pichu pichu pi pi pi pi pi pi pi pi pi pi pi pi pi pi pi pikachu pipi pikachu pi pi pi pikachu ka ka ka ka ka ka pikachu ka ka ka ka ka ka ka ka pikachu pipi pi pikachu pipi pikachu"
+pikalang.evaluate(msg)
