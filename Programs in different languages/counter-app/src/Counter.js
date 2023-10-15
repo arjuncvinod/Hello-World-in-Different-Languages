7af21f6c401701e8d0914e0104f4685628625259
akemim12 <91174529+akemim12@users.noreply.github.com>
2023-10-15 16:29:08 -0700

    adding a counter react tool

---
+++ Programs in different languages/counter-app/src/Counter.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+}
+
+export default Counter;
