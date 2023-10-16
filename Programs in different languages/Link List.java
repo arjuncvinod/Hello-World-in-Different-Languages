39876344e8fcffbecc42215c2e08ab0889a09ee7
Likhon <105287746+brlikhon@users.noreply.github.com>
2023-10-16 22:28:18 +0600

    Create Link List.java

---
+++ Programs in different languages/Link List.java
@@ -0,0 +1,28 @@
+public class LikhList {
+    public static void main(String[] args) {
+        String sentence = "hello hackers! welcome to hacktoberfest 2023";
+        String capitalized = capitalizeFirstLetter(sentence);
+        System.out.println(capitalized);
+    }
+
+    public static String LikhList(String input) {
+        if (input == null || input.isEmpty()) {
+            return input;
+        }
+
+        String[] words = input.split("\\s"); // Split the input string into words
+        StringBuilder result = new StringBuilder();
+
+        for (String word : words) {
+            if (word.length() > 0) {
+                // Capitalize the first letter of the word and append the rest of the word
+                result.append(Character.toUpperCase(word.charAt(0)));
+                if (word.length() > 1) {
+                    result.append(word.substring(1));
+                }
+                result.append(" ");
+            }
+        }
+        return result.toString();
+    }
+}
