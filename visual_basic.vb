7179dce635b30ab11b096b45856da0e5bc8bbdd6
Ankit Raj <97803435+ankitraj477@users.noreply.github.com>
2023-10-19 23:17:54 +0530

    visual_basic.vb

---
+++ visual_basic.vb
@@ -0,0 +1,9 @@
+Imports System
+
+Module Module1
+   Sub Main()
+     Console.WriteLine("Hello World!")
+     Console.WriteLine("Press Enter Key to Exit.")
+     Console.ReadLine()
+   End Sub
+End Module
