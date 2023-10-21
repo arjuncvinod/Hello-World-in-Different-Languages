fcc97ce4d4371c5211e5185970b713cf8a6cef63
Sushant Gupta <sushantggupta@gmail.com>
2023-10-22 00:18:04 +0530

    Updated hello world in cpp

--- C++.cpp
+++ C++.cpp
@@ -1,8 +1,13 @@
 // Hello World in C++ Program
 
 #include <iostream>
+#include <string>
+
 using namespace std;
 int main() {
-    cout << "Hello World!";
+    cout << "What is your name <3 :";
+    string s;
+    cin >> s;
+    cout << "You are my world\nHello "<< s << endl ; 
     return 0;
 }
