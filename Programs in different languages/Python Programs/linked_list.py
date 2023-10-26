fae30b9ba0a9d5717ac39f5db1e5e18f25a52d7f
Lakshman142 <96649441+Lakshman142@users.noreply.github.com>
2023-10-26 16:15:39 +0530

    Create linked_list.py

---
+++ Programs in different languages/Python Programs/linked_list.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+# Creating a linked list
+my_linked_list = LinkedList()
+my_linked_list.append(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+
+# Displaying the linked list
+my_linked_list.display()
