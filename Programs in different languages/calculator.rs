cd0fdc6a723f4c045c818ed6f1210f1e76a9993c
Shashidhar B M <112196062+ShashidharM0118@users.noreply.github.com>
2023-10-27 09:30:39 +0530

    Simple calculator in rust

---
+++ Programs in different languages/calculator.rs
@@ -0,0 +1,17 @@
+fn main() {
+    // Define variables
+    let num1 = 10;
+    let num2 = 5;
+    
+    // Perform some calculations
+    let sum = num1 + num2;
+    let difference = num1 - num2;
+    let product = num1 * num2;
+    let quotient = num1 / num2;
+    
+    // Print the results
+    println!("Sum: {}", sum);
+    println!("Difference: {}", difference);
+    println!("Product: {}", product);
+    println!("Quotient: {}", quotient);
+}
