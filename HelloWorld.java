04a9e8129cd1fe2dcf4614fd7e780d1cf83b7a43
Arjun C Vinod <arjuncvinod007@gmail.com>
2024-02-08 21:47:35 +0530

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-class Hacktoberfest2023{
-    public static void main(String args[])
-    {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Hello World");
-    }
-}
